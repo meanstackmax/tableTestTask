53c21d635cf55dc4b62402b37670e5c01365fb38
maxim_s <meanstackmax@gmail.com>
2018-01-18 10:27:19 +0200

    refactor table

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -42,88 +42,12 @@ const table = {
 
 export default class Index extends Component {
 
-    state = {
-        columns: [],
-        dataSource: []
-    }
-
-    componentDidMount() {
-        const dataSource = table.dataSource
-        const columns = table.columns
-
-        dataSource.forEach((item, index) => {
-            while (item.length < columns.length) {
-                item.push("")
-            }
-        })
-        this.setState({dataSource, columns})
-    }
-
-    editCell({target}) {
-
-        const rowIndex = target.dataset.row
-        const columnIndex = target.dataset.column
-        const fieldValue = target.value
-
-        const {dataSource} = this.state
-
-        if (target.dataset.select) {
-            dataSource[rowIndex][columnIndex] = {
-                selected: fieldValue,
-                items: JSON.parse(target.dataset.select)
-            }
-        } else {
-            dataSource[rowIndex][columnIndex] = fieldValue;
-        }
-
-
-        this.setState({dataSource})
-    }
-
-    editColumn({target}) {
-
-        const rowIndex = target.dataset.key
-        const fieldValue = target.value
-
-        const {columns} = this.state
-
-        columns[rowIndex] = fieldValue;
-
-        this.setState({columns})
-    }
-
-    handleAddColumn() {
-        const {columns, dataSource} = this.state
-
-        columns.push('column name')
-
-        dataSource.forEach((item, index) => {
-            item.push("")
-            return item
-        })
-
-        this.setState({columns, dataSource})
-    }
-
-    handleAddRow() {
-        const {columns, dataSource} = this.state
-        dataSource.push(Array(columns.length).fill(""))
-        this.setState({dataSource})
-    }
-
 
     render() {
-        const {dataSource} = this.state
-        console.log(dataSource)
-
         return <Layout>
             <DinamicTable
-                data={this.state.dataSource}
-                onEditColumn={this.editColumn.bind(this)}
-                onEditCell={this.editCell.bind(this)}
-                columns={this.state.columns}
-                onAddColumn={this.handleAddColumn.bind(this)}
-                onAddRow={this.handleAddRow.bind(this)}
+                data={table.dataSource}
+                columns={table.columns}
             />
         </Layout>
     }
